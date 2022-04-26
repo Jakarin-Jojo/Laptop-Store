eccca800efe9e3e376c5bcce5ea2ff950d073c80
Jakarin Chujan <jojogamer67@gmail.com>
2022-04-26 22:37:47 +0700

    Create Customer model

---
+++ model/customer_model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, Text
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class CustomerModel(Base):
+    __tablename__ = "customer"
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, nullable=False)
+    firstname = Column(Text, nullable=False)
+    lastname = Column(Text, nullable=False)
+    gender = Column(Text, nullable=False)
+    laptop_id = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Customer(id = {self.id}, firstname= {self.firstname}, lastname={self.lastname}, gender={self.gender},"\
+               f" laptop_id ={self.laptop_id})>"
