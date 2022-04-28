edbfb990d3d2fc37a7b5de3df047cd63a2c055ee
Jakarin Chujan <jojogamer67@gmail.com>
2022-04-28 19:22:31 +0700

    Modify a LaptopDB

--- utilities/session.py
+++ utilities/session.py
@@ -8,8 +8,8 @@ from utilities.dao.laptop_dao import LaptopDao
 class LaptopDB:
 
     def __init__(self):
-        self.__engine = create_engine('sqlite:///laptop_store.db', echo=True)
-        session = sessionmaker(bind=self.__engine)
+        engine = create_engine('sqlite:///laptop_store.db', echo=True)
+        session = sessionmaker(bind=engine)
         self.__session = session()
 
     def customer(self):
